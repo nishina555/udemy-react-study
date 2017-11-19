8935701df2355a0ed12f89c2936fe275bcc11d5a
nishina555 <toshiharu.nishina@spacemarket.co.jp>
2017-11-19 16:40:33 +0900

    create search form with query by react-router

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
+import queryString from 'query-string';
 
 import SearchForm from './SearchForm';
 import GeocodeResult from './GeocodeResult';
@@ -17,12 +18,29 @@ class SearchPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      place: this.getPlaceParam() || '東京タワー',
       location: {
         lat: 35.6585805,
         lng: 139.7454329,
       },
       sortKey: 'price',
     };
+  }
+
+  componentDidMount() {
+    const place = this.getPlaceParam();
+    if (place) {
+      this.startSearch(place);
+    }
+  }
+
+  getPlaceParam() {
+    const params = queryString.parse(this.props.location.search);
+    const place = params.place;
+    if (place && place.length > 0) {
+      return place;
+    }
+    return null;
   }
 
   setErrorMessage(message) {
@@ -35,9 +53,18 @@ class SearchPage extends Component {
     });
   }
 
-  handlePlaceSubmit(place) {
-    this.props.history.push(`/?query=${place}`);
-    geocode(place)
+  handlePlaceChange(place) {
+    this.setState({ place });
+  }
+
+  handlePlaceSubmit(e) {
+    e.preventDefault();
+    this.props.history.push(`/?place=${this.state.place}`);
+    this.startSearch();
+  }
+
+  startSearch() {
+    geocode(this.state.place)
       .then(({ status, address, location }) => {
         switch (status) {
           case 'OK': {
@@ -73,7 +100,11 @@ class SearchPage extends Component {
     return (
       <div className="search-page">
         <h1 className="app-title">ホテル検索</h1>
-        <SearchForm onSubmit={place => this.handlePlaceSubmit(place)} />
+        <SearchForm
+          place={this.state.place}
+          onPlaceChange={place => this.handlePlaceChange(place)}
+          onSubmit={(e) => this.handlePlaceSubmit(e)}
+        />
         <div className="result-area">
           <Map location={this.state.location} />
           <div className="result-right">
@@ -96,6 +127,7 @@ class SearchPage extends Component {
 
 SearchPage.propTypes = {
   history: PropTypes.shape({ push: PropTypes.func }).isRequired, // pushという関数を持っているオブジェクト
+  location: PropTypes.shape({ search: PropTypes.string }).isRequired,
 };
 
 export default SearchPage;
