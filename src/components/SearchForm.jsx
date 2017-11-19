5c5da75795ff30d3664c31909154d7a5e77d9074
nishina555 <toshiharu.nishina@spacemarket.co.jp>
2017-11-19 16:41:09 +0900

    fix bug

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const SearchForm = props => (
-  <form className="search-form" onSubmit={(e) => this.props.onSubmit(e)}>
+  <form className="search-form" onSubmit={(e) => props.onSubmit(e)}>
     <input
       className="place-input"
       type="text"
       size="30"
       value={props.place}
-      onChange={e => this.onPlaceChange(e.target.value)}
+      onChange={e => props.onPlaceChange(e.target.value)}
     />
-    <input className="submit-button" type="submit" value="Search" />
+    <input className="submit-button" type="submit" value="検索" />
   </form>
 );
 
