30ac0c6d0c6e1282d4081a1605cf331e1f0425e5
nishina555 <toshiharu.nishina@spacemarket.co.jp>
2017-11-20 02:21:02 +0900

    serch with redux

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,35 @@
 // 純粋関数
 // 前回のstateとactionをもらって新しいstateを返す
-export default (state = { place: 'hoge' }, action) => {
-  console.log(action);
+// 肥大化するのでreducerをkeyごとに分割していく
+import { combineReducers } from 'redux';
+
+const place = (state = '東京タワー', action) => {
   switch (action.type) {
     case 'CHANGE_PLACE':
       // actionのplaceの内容をsetStateしたいがそのようにすると純粋関数じゃ無くなってしまうのでassignを利用
-      return Object.assign({}, state, { place: action.place });
+      return action.place;
     default:
       return state;
   }
 };
+
+const geocodeResult = (
+  state = {
+    address: '',
+    location: { lat: 35.6585805, lng: 139.7454329 },
+  },
+  action,
+) => {
+  switch (action.type) {
+    case 'GEOCODE_FETCHED':
+      // actionのplaceの内容をsetStateしたいがそのようにすると純粋関数じゃ無くなってしまうのでassignを利用
+      return {
+        address: action.address,
+        location: action.location,
+      };
+    default:
+      return state;
+  }
+};
+
+export default combineReducers({ place, geocodeResult });
